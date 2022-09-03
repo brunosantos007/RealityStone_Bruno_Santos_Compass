10f1856a64fb1156ab820f4b493dd8cc9024679c
Bruno Santos <brunogsantoss@outlook.com>
2022-09-03 15:19:29 -0300

    Novas ideias

--- Gem/specs/calculadora_spec.rb
+++ Gem/specs/calculadora_spec.rb
@@ -22,6 +22,18 @@ describe("Calculadora :") do
         expect(calculadora.sum(first_number,second_number)).to eq 10
     end
 
+    it("SOMA - Deve somar dois números inteiros negativos.") do 
+        first_number = -5
+        second_number = -5
+        expect(calculadora.sum(first_number,second_number)).to eq -10
+    end
+
+    # it("SOMA - A resposta da soma de dois números inteiros negativos deve ser negativa.") do 
+    #     first_number = -5
+    #     second_number = -5
+    #     expect(calculadora.sum(first_number,second_number)).to output(/-10/)
+    # end
+
     it("SOMA - Deve somar dois números inteiros positivos com casas decimais.") do 
         first_number = 50.5
         second_number = 70.6
@@ -42,7 +54,7 @@ describe("Calculadora :") do
         expect(calculadora.sum(first_number,second_number)).to be_kind_of(Numeric)
     end
 
-     "-------------ERRADOS-------------"
+    "-------------ERRADOS-------------"
 
     #  it("SOMA - Um dos números não deve ser nulo") do 
     #     first_number = 5
@@ -55,7 +67,188 @@ describe("Calculadora :") do
     #     second_number = "5"
     #     expect(calculadora.sum(first_number,second_number)).to be_kind_of(Numeric)
     # end
+
+    #  it("SOMA - O resultado da soma deve ser do tipo Float.") do 
+    #      first_number = 50.0
+    #      second_number = "70.0"
+    #      # Resposta = 121.1
+    #      expect(calculadora.sum(first_number,second_number)).to be_kind_of(Float)
+    #  end
+
+# TESTE SUBTRAÇÃO
+
+    "-------------CORRETOS-------------"
+
+    it("SUBTRAÇÃO - Um dos números não deve ser nulo.") do 
+        first_number = 5
+        second_number = 5
+        expect(calculadora.subtraction(first_number,second_number)).not_to be_nil
+    end
+
+    it("SUBTRAÇÃO - Deve subtrair dois números inteiros.") do 
+        first_number = 10
+        second_number = 5
+        expect(calculadora.subtraction(first_number,second_number)).to eq 5
+    end
+
+    it("SUBTRAÇÃO - Deve subtrair dois números inteiros com casas decimais.") do 
+         first_number = 50.5
+         second_number = 70.6
+         expect(calculadora.subtraction(first_number,second_number)).to eq -20.1
+    end
+
+    it("SUBTRAÇÃO - O resultado da subtração com casas decimais deve ser do tipo Float") do 
+        first_number = 50.5
+        second_number = 70.6
+        expect(calculadora.subtraction(first_number,second_number)).to be_kind_of(Float)
+   end
+
+    it("SUBTRAÇÃO - Subtrair dois número sendo o primeiro menor que o segundo.") do 
+        first_number = 20
+        second_number = 50
+        expect(calculadora.subtraction(first_number,second_number)).to eq -30
+    end
+
+    "-------------ERRADOS-------------"
+
+    # it("SUBTRAÇÃO - Um dos números não deve ser nulo.") do 
+    #     first_number = 5
+    #     second_number = 
+    #     expect(calculadora.subtraction(first_number,second_number)).not_to be_nil
+    # end
     
+
+# TESTE DIVISÃO
+
+    "-------------CORRETOS-------------"
+
+    it("DIVISÃO - Um dos números não deve ser nulo.") do 
+        first_number = 5
+        second_number = 5
+        expect(calculadora.division(first_number,second_number)).not_to be_nil
+    end
+
+    it("DIVISÃO - Deve dividir dois números inteiros positivos.") do 
+        first_number = 10
+        second_number = 5
+        expect(calculadora.division(first_number,second_number)).to eq 2
+    end
+
+    it("DIVISÃO - Deve dividir dois números inteiros sendo um negativo.") do 
+        first_number = -10
+        second_number = 5
+        expect(calculadora.division(first_number,second_number)).to eq -2
+    end
+
+    it("DIVISÃO - Deve dividir dois números inteiros sendo os dois negativos.") do 
+        first_number = -10
+        second_number = -5
+        expect(calculadora.division(first_number,second_number)).to eq 2
+    end
+
+    it("DIVISÃO - Deve dividir dois números inteiros com casas decimais.") do 
+        first_number = 70.6
+        second_number = 50.5
+        expect(calculadora.division(first_number,second_number)).to eq 1.4
+   end
+
+   it("DIVISÃO - O resultado da divisão dos numeros com casas decimais deve ser do tipo FLOAT") do 
+        first_number = 70.6
+        second_number = 50.5
+        expect(calculadora.division(first_number,second_number)).to be_kind_of(Float)
+    end
+
+    it("DIVISÃO - O resultado da divisão dos numeros inteiros deve ser do tipo Numerico") do 
+        first_number = 70.6
+        second_number = 50.5
+        expect(calculadora.division(first_number,second_number)).to be_kind_of(Numeric)
+    end
+
+# TESTE MULTIPLICAÇÃO
+
+    "-------------CORRETOS-------------"
+
+    it("MULTIPLICAÇÃO - Um dos números não deve ser nulo.") do 
+        first_number = 5
+        second_number = 5
+        expect(calculadora.multiplication(first_number,second_number)).not_to be_nil
+    end
+
+    it("MULTIPLICAÇÃO - Deve multiplicar dois números inteiros positivos.") do 
+        first_number = 10
+        second_number = 5
+        expect(calculadora.multiplication(first_number,second_number)).to eq 50
+    end
+
+    it("MULTIPLICAÇÃO - Deve multiplicar dois números inteiros sendo um negativo.") do 
+        first_number = -10
+        second_number = 5
+        expect(calculadora.multiplication(first_number,second_number)).to eq -50
+    end
+
+    it("MULTIPLICAÇÃO - Deve multiplicar dois números inteiros sendo os dois negativos.") do 
+        first_number = -10
+        second_number = -5
+        expect(calculadora.multiplication(first_number,second_number)).to eq 50
+    end
+
+    it("MULTIPLICAÇÃO - Deve multiplicar dois números inteiros com casas decimais.") do 
+        first_number = 70.6
+        second_number = 50.5
+        expect(calculadora.multiplication(first_number,second_number)).to eq 3565.3
+   end
+
+   it("MULTIPLICAÇÃO - O resultado da multiplicação dos numeros com casas decimais deve ser do tipo FLOAT") do 
+        first_number = 70.6
+        second_number = 50.5
+        expect(calculadora.multiplication(first_number,second_number)).to be_kind_of(Float)
+    end
+
+    it("MULTIPLICAÇÃO - O resultado da multiplicação dos numeros inteiros deve ser do tipo Numerico") do 
+        first_number = 70
+        second_number = 50
+        expect(calculadora.multiplication(first_number,second_number)).to be_kind_of(Numeric)
+    end 
+
+# TESTE RESTO DA DIVISÃO
+
+    "-------------CORRETOS-------------"
+
+    it("RESTO - A escolha de um dos números não deve ser nula.") do
+        first_number = 10
+        second_number = 5
+        expect(calculadora.restoDivisao(first_number,second_number)).not_to be_nil
+    end
+
+    it("RESTO - Deve mostrar o resto da divisão entre dois numeros inteiros.") do
+        first_number = 10
+        second_number = 5
+        expect(calculadora.restoDivisao(first_number,second_number)).to be == 0
+    end
+
+    it("RESTO - Deve mostrar o resto da divisão entre dois números inteiros sendo um negativo.") do
+        first_number = -12
+        second_number = 5
+        expect(calculadora.restoDivisao(first_number,second_number)).to eq 3
+    end
+
+    it("RESTO - Deve mostrar o resto da divisão entre dois números inteiros sendo os dois números negativos.") do
+        first_number = -12
+        second_number = -5
+        expect(calculadora.restoDivisao(first_number,second_number)).to eq -2
+    end
+
+    it("RESTO - Deve mostrar o resto da divisão entre dois números inteiros com casas decimais.") do
+        first_number = 3.4
+        second_number = 2
+        expect(calculadora.restoDivisao(first_number,second_number)).to eq 1.4
+    end
+
+# TESTE RAIZ QUADRADA
+
+    "-------------CORRETOS-------------"
+
+   
 
 end
 
@@ -79,6 +272,8 @@ end
 # # TESTE RESTO DA DIVISÃO
 
 # # TESTE RAIZ QUADRADA
+
+# TESTE DO PI
 
 # it("Deve calcular a raiz quadrada do número") do 
 #     expect(calculadora.raizQuadrada(25)).to eq 5
--- Gem/src/calculadora.rb
+++ Gem/src/calculadora.rb
@@ -1,66 +1,29 @@
-#  class Calculadora
-
-
-#      def escolha
-#             puts "1 - SOMA"
-#             puts "2 - SUBTRAÇÃO"
-#             puts "3 - DIVISÃO"
-#             puts "4 - MULTIPLICAÇÃO"
-#             print "Escolha sua opcao: "
-#             opcao = gets.chomp.to_i
-        
-
-#          if opcao <= 0 || opcao >= 5
-#              puts "Valor indisponivel"
-#             else
-#                 print "Insira um número: "
-#                 first_number = gets.chomp.to_i
-#                 print "Insira outro número: "
-#                 second_number = gets.chomp.to_i
-
-#                 case opcao
-#                 when 1 
-#                     soma(first_number,second_number)
-#                 end
-#             end
-#      end
-
-#      def soma(first_number,second_number)
-#          result = first_number + second_number
-#          result
-#      end 
-
-#     end 
-
-#  teste = Calculadora.new
-
-#  teste.escolha
-
- class Calculadora
+class Calculadora
 
     
 
      def sum(first_number,second_number)
-        result = first_number + second_number
-        result
+        first_number + second_number
      end
 
      def subtraction(first_number,second_number)
-        first_number - second_number
+        result = first_number - second_number
+        result.round(1)
     end
 
     def division(first_number,second_number)
-        first_number / second_number
+        result = first_number / second_number
+        result.round(1)
     end
 
     def multiplication(first_number,second_number)
-        first_number * second_number
-        
+        result = first_number * second_number
+        result.round(1)
     end
 
-    def restoDivisão(first_number,second_number)
-        result = first_number % second_number
-        result.round(2)
+    def restoDivisao(first_number,second_number)
+        first_number % second_number
+        # result.round(2)
     end
 
     def raizQuadrada(first_number)
